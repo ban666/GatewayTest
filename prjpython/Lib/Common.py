8110f205bc8c34c7b685ae04e141d366538129f7
ban666 <ban666@qq.com>
2015-11-18 17:18:19 +0800

    add more function

--- prjpython/Lib/Common.py
+++ prjpython/Lib/Common.py
@@ -108,31 +108,54 @@ def secondsToTime(iItv):
     else:
         return "[InModuleError]:itv2time(iItv) invalid argument type"
 
-def generate_report(fname,sn,login_result,version_check_result,status_check_result,control_test_result,env_clear_result,case_result):
+def generate_report(fname,sn,login_result,version_check_result,status_check_result,control_test_result,env_clear_result,case_result,s_time,e_time):
     result_dict = {1:'OK',0:'Failed',-1:'Block'}
     data = [login_result,version_check_result,status_check_result,control_test_result,env_clear_result,case_result]
     data = [result_dict[x] for x in data]
     data.insert(0,sn)
+    data.append(s_time)
+    data.append(e_time)
     ExcelWriteForAna(fname,data)
 
-def ExcelWriteForAna(fname,data):
+def ExcelWriteForAna(fname,content):
+    if not os.path.exists(fname):
+        xlsxwriter.Workbook(fname)
+    columnlist=[u'网关sn',u'登录测试',u'版本检查测试',u'状态信息测试',u'控制测试',u'环境恢复',u'测试结果',u'开始时间',u'结束时间']
+    data = xlrd.open_workbook(fn)            # 打开fname文件
+    data.sheet_names()                          # 获取xls文件中所有sheet的名称
+    table = data.sheet_by_index(0)              # 通过索引获取xls文件第0个sheet
+    nrows = table.nrows                         # 获取table工作表总行数
+    ncols = table.ncols
+    tlist = []
+    for i in xrange(nrows):              #设定第i行单元格属性，高度为22像素，行索引从0开始
+        tlist.append([])
+        for j in  xrange(ncols):
+            tlist[i].append(table.cell_value(i,j,))
+    tlist.append(content)
+    if len(tlist)>1:
+        tlist = tlist[1:]
+    print tlist
     wb = xlsxwriter.Workbook(fname)
     ws = wb.add_worksheet()
-    columnlist=[u'网关sn',u'登录测试',u'版本检查测试',u'状态信息测试',u'控制测试',u'环境恢复',u'测试结果']
-     
-    ws.set_column(0,6,22)
+    ws.set_column(0,8,22)
     #column
-    for i in range(7):
+    for i in range(9):
         ws.write(0,i,columnlist[i])
-    
-    
-    for j in range(7):
-        #print data[i][j]
-        ws.write(1,j,str(data[j]).decode('utf-8'))
+    if tlist is not None:
+        for i in range(len(tlist)):
+            for j in range(9):
+                ws.write(i+1,j,str(tlist[i][j]).decode('utf-8'))
 
     wb.close()
 
 if __name__ == '__main__':
     fn = 'e:/3.xlsx'
-
-    generate_report(fn,123,1,1,0,-1,-1,0)
+    st = '2015-11-18 16:24:49'
+    et = '2015-11-18 16:26:52'
+    generate_report(fn,123,1,1,0,-1,-1,0,st,et)
+    data = xlrd.open_workbook(fn)            # 打开fname文件
+    data.sheet_names()                          # 获取xls文件中所有sheet的名称
+    table = data.sheet_by_index(0)              # 通过索引获取xls文件第0个sheet
+    nrows = table.nrows                         # 获取table工作表总行数
+    ncols = table.ncols                         # 获取table工作表总列数
+    print nrows
--- prjpython/Lib/appium_lib.py
+++ prjpython/Lib/appium_lib.py
@@ -3,7 +3,7 @@
 # @Author: liaoben
 # @Date:   2015-10-27 10:14:36
 # @Last Modified by:   liaoben
-# @Last Modified time: 2015-11-17 15:10:27
+# @Last Modified time: 2015-11-18 15:25:10
 
 from appium import webdriver
 import time,sys
@@ -45,7 +45,7 @@ def login_to_page(driver,username,password,sn):
     login_result = login(driver,username,password)
     if login_result != 1:
         return 0
-    time.sleep(5)
+    time.sleep(8)
     cur_activity=driver.current_activity
     if cur_activity=='.ui.MainActivity':
         try:
@@ -96,7 +96,7 @@ def get_version(driver):
         software_ver = softver.split(':')[-1]
         driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
         driver.find_elements_by_id('com.jlkj.janlinker:id/tv_title')[0].click()
-        result = [1,gateway_ver[-5:],software_ver[-5:]]
+        result = gateway_ver[-5:]
     except Exception as e:
         func_name = sys._getframe().f_code.co_name
         print func_name+'_Error:',e
@@ -110,19 +110,21 @@ def clear_device(driver):
         menu_result = open_menu(driver)
         if menu_result[0] != 1:
             return 0
-        sys_info = menu_result[1][-2].click()
+        sys_info = menu_result[1][5].click()
         driver.find_elements_by_id('com.jlkj.janlinker:id/icondetail')[0].click()
         device_num = 0
         try:
             device_num = len(driver.find_elements_by_id('com.jlkj.janlinker:id/tv_mac'))
         except:
             pass
+        driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
         for device in range(device_num):
-            driver.swipe(810,150,110,160,1000);
+            driver.find_elements_by_id('com.jlkj.janlinker:id/icondetail')[0].click()
+            driver.swipe(810,150,110,160,1000)
             driver.execute_script("mobile: tap", {"touchCount":"1", "x":845, "y":163})
             driver.find_element_by_id('com.jlkj.janlinker:id/positiveButton').click()
             time.sleep(5)
-        driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
+            driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
         driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
         driver.find_elements_by_id('com.jlkj.janlinker:id/tv_title')[0].click()
         result = 1
@@ -158,6 +160,38 @@ def add_area(driver,areas):
     finally:
         return result
 
+def add_devices(driver,devices):
+    result = -1
+    try:
+        menu_result = open_menu(driver)
+        if menu_result[0] != 1:
+            return 0
+        sys_settings = menu_result[1][4].click()
+        init_settings = driver.find_elements_by_id('com.jlkj.janlinker:id/textitem')[0].click()
+        for i in range(len(devices)):
+            driver.find_element_by_id('com.jlkj.janlinker:id/adddeivces').click()
+            driver.execute_script("mobile: tap", {"touchCount":"1", "x":870, "y":110})
+            time.sleep(5)
+            driver.find_element_by_id('com.jlkj.janlinker:id/ercode_scan_by_hand').click()
+            area_name = driver.find_element_by_id('com.jlkj.janlinker:id/message').send_keys(devices[i][0])
+            driver.find_element_by_id('com.jlkj.janlinker:id/positiveButton').click()
+            driver.find_element_by_id('com.jlkj.janlinker:id/btn_queding').click()
+            area_name = driver.find_element_by_id('com.jlkj.janlinker:id/message').send_keys(devices[i][1])
+            driver.find_element_by_id('com.jlkj.janlinker:id/positiveButton').click()
+            #device[i][2]:1 智能家电 2 智能灯光
+            driver.find_element_by_id('android:id/button'+devices[i][2]).click()
+        driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
+        driver.find_element_by_id('com.jlkj.janlinker:id/iv_back').click()
+        driver.find_elements_by_id('com.jlkj.janlinker:id/tv_title')[0].click()
+        result = 1
+    except Exception as e:
+        func_name = sys._getframe().f_code.co_name
+        print func_name+'_Error:',e
+        result = 0
+    finally:
+        return result
+
+
 def del_area(driver,areas_num):
     result = -1
     try:
@@ -168,7 +202,7 @@ def del_area(driver,areas_num):
         init_settings = driver.find_elements_by_id('com.jlkj.janlinker:id/textitem')[0].click()
         add_area = driver.find_elements_by_id('com.jlkj.janlinker:id/showpop')[0].click()
         for area in range(areas_num):
-            driver.swipe(810,150,110,160,1000);
+            driver.swipe(810,150,110,160,1000)
             driver.execute_script("mobile: tap", {"touchCount":"1", "x":845, "y":163})
             driver.find_element_by_id('com.jlkj.janlinker:id/positiveButton').click()
         driver.execute_script("mobile: tap", {"touchCount":"1", "x":873, "y":100})
@@ -191,10 +225,14 @@ def clear_area(driver):
             return 0
         sys_settings = menu_result[1][4].click()
         init_settings = driver.find_elements_by_id('com.jlkj.janlinker:id/textitem')[0].click()
-        areas_num = len(driver.find_elements_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.widget.TextView'))
+        area_num = 0
+        try:
+            areas_num = len(driver.find_elements_by_xpath('//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.View/android.widget.RelativeLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.HorizontalScrollView/android.widget.LinearLayout/android.widget.TextView'))
+        except:
+            pass
         add_area = driver.find_elements_by_id('com.jlkj.janlinker:id/showpop')[0].click()
         for area in range(areas_num):
-            driver.swipe(810,150,110,160,1000);
+            driver.swipe(810,150,110,160,1000)
             driver.execute_script("mobile: tap", {"touchCount":"1", "x":845, "y":163})
             driver.find_element_by_id('com.jlkj.janlinker:id/positiveButton').click()
         driver.execute_script("mobile: tap", {"touchCount":"1", "x":873, "y":100})
@@ -209,19 +247,6 @@ def clear_area(driver):
     finally:
         return result
 
-def open_light_module(driver):
-    result = -1
-    try:
-        driver.find_element_by_id('com.jlkj.janlinker:id/item_name')[0].click()
-        menu.click()
-        menu_content = driver.find_elements_by_id('com.jlkj.janlinker:id/tv_title')
-        result = [1,menu_content]
-    except Exception as e:
-        func_name = sys._getframe().f_code.co_name
-        print func_name+'_Error:',e
-        result = 0
-    finally:
-        return result
 
 def get_all_opendevice_in_light(driver):
     driver.find_elements_by_id('com.jlkj.janlinker:id/item_name')[1].click()
@@ -231,7 +256,6 @@ def get_all_opendevice_in_light(driver):
         try:
             r='-1'
             txt = i.text.encode('utf-8')
-            print txt.decode('utf-8')
             if txt == '①开':
                 r = '1'
                 result.append(r)
@@ -241,7 +265,6 @@ def get_all_opendevice_in_light(driver):
             elif txt == '③开':
                 r = '3'
                 result.append(r)
-            print r
         except Exception as e :
             print e
     return result
@@ -260,3 +283,8 @@ def control_device_in_device(driver):
             result = result * 0
             break
     return result
+
+def wait_for_access(driver,times=120):
+    for i in range(4):
+        time.sleep(times/4)
+        driver.find_elements_by_id('com.jlkj.janlinker:id/item_name')[0].click()
